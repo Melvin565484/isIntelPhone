65a5acf5f481fcd7d71823fb4829160e544610f2
Julio Sanchez <juliocc0502@gmail.com>
2023-06-06 17:33:58 -0600

    Actualizacion Roles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,38 @@
 import './bootstrap';
+
+new Vue({
+    el: '#app',
+    data: {
+      newUser: {
+        name: '',
+        email: '',
+        selectedRoles: []
+      },
+      users: [],
+      roles: [
+        { label: 'Administrador', value: 'admin' },
+        { label: 'Cliente', value: 'cliente' },
+        { label: 'Gerente', value: 'gerente' },
+        { label: 'Supervisor', value: 'supervisor' },
+        { label: 'Preventa', value: 'preventa' },
+        { label: 'Repartidor', value: 'repartidor' }
+      ]
+    },
+    methods: {
+      addUser() {
+        this.users.push({
+          name: this.newUser.name,
+          email: this.newUser.email,
+          selectedRoles: [...this.newUser.selectedRoles]
+        });
+  
+        this.newUser.name = '';
+        this.newUser.email = '';
+        this.newUser.selectedRoles = [];
+      },
+      removeUser(index) {
+        this.users.splice(index, 1);
+      }
+    }
+  });
+  
--- resources/views/adminroles.php
+++ resources/views/adminroles.php
@@ -1,27 +1,47 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Gestión de Roles</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gestión de usuarios</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
 </head>
 <body>
-    <h1>Gestión de Roles</h1>
-
-    <form action="procesar_formulario.php" method="post">
-        <label for="nombre">Usuario:</label>
-        <input type="text" name="nombre" id="nombre" required><br><br>
-
-        <label for="email">Correo electrónico:</label>
-        <input type="email" name="email" id="email" required><br><br>
-
-        <label for="rol">Rol:</label>
-        <select name="rol" id="rol">
-            <option value="admin">Crear</option>
-            <option value="editor">Modificar</option>
-            <option value="usuario">Visualizar</option>
-            <option value="usuario">Todas las autorizaciones</option>
-         </select><br><br>
-
-        <input type="submit" value="Guardar">
+  <div id="app" class="container">
+    <h1>Gestión de usuarios</h1>
+    <form @submit.prevent="addUser">
+      <div class="form-group">
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" class="form-control" v-model="newUser.name">
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" class="form-control" v-model="newUser.email">
+      </div>
+      <div class="form-group">
+        <label>Rol:</label>
+        <div class="form-check" v-for="(role, index) in roles" :key="index">
+          <input type="checkbox" class="form-check-input" :id="role.value" v-model="newUser.selectedRoles" :value="role.value">
+          <label class="form-check-label" :for="role.value"> Administrador </label>
+        </div>
+        <div class="form-check" v-for="(role, index) in roles" :key="index">
+          <input type="checkbox" class="form-check-input" :id="role.value" v-model="newUser.selectedRoles" :value="role.value">
+          <label class="form-check-label" :for="role.value"> Gerente </label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Agregar usuario</button>
     </form>
+
+    <h2>Usuarios</h2>
+    <ul class="list-group">
+      <li v-for="(user, index) in users" :key="index" class="list-group-item">
+        {{ user.name }} - {{ user.email }} - {{ user.selectedRoles.join(', ') }}
+        <button @click="removeUser(index)" class="btn btn-danger">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+
+  <script src="app.js"></script>
 </body>
 </html>
